22870a65b1994311e87f152259a9bb9e370f9952
Obbizzy <115794709+Obbiz@users.noreply.github.com>
2025-01-30 16:16:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,9 +43,9 @@ const chalk = require('chalk');
 //~~~~~~~~< Owner Information>~~~~~~~~~~~~~~\\
 global.ytname = process.env.YT_NAME || "YT: S4 SALMAN YT";
 
-global.socialm = process.env.GITHUB_USERNAME || "GitHub: salmanytofficial";
+global.socialm = process.env.GITHUB_USERNAME || "GitHub: Obbiz";
 
-global.location = process.env.LOCATION || "Pakistan, Lahore, Sabzazar";
+global.location = process.env.LOCATION || "Nigeria, Kano, Badawa";
 
 
 //~~~~~~~~<Session and Bot Details>~~~~~~~~~~~~~\\
@@ -53,15 +53,15 @@ global.SESSION_ID = process.env.SESSION_ID || '';
 
 global.botname = process.env.BOT_NAME || 'XLICON-V4-MD';
 
-global.ownernumber = [process.env.OWNER_NUMBER || '923184070915'];
+global.ownernumber = [process.env.OWNER_NUMBER || '2348136118877'];
 
-global.ownername = process.env.OWNER_NAME || '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙';
+global.ownername = process.env.OWNER_NAME || 'Obiii';
 
 
 //~~~~~~<Website and Social Links>~~~~~~~~~~\\
 global.websitex = process.env.WEBSITE_URL || "https://youtube.com/@s4salmanyt";
 
-global.wagc = process.env.WHATSAPP_CHANNEL || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f";
+global.wagc = process.env.WHATSAPP_CHANNEL || "ll";
 
 
 //~~~~~~~~<Theme and Miscellaneous>~~~~~~~~~~\\
@@ -71,17 +71,17 @@ global.wm = process.env.WATERMARK || "Xlicon Bot Inc.";
 
 global.botscript = process.env.SCRIPT_LINK || 'https://github.com/salmanytofficial/XLICON-V4-MD';
 
-global.packname = process.env.PACK_NAME || "𝘼𝙃𝙃𝙈𝙄 𝙆𝙐𝙉";
+global.packname = process.env.PACK_NAME || "By";
 
-global.author = process.env.AUTHOR_NAME || "MΛDΣ BY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿";
+global.author = process.env.AUTHOR_NAME || "Obiii";
 
-global.creator = process.env.CREATOR_NUMBER || "923184070915@s.whatsapp.net";
+global.creator = process.env.CREATOR_NUMBER || "2348136118877@s.whatsapp.net";
 
 
 //~~~~~~~~~~~~~<Bot Settings>~~~~~~~~~~~~~~~\\
 global.xprefix = process.env.PREFIX || '.';
 
-global.premium = [process.env.PREMIUM_NUMBER || '923184070915'];
+global.premium = [process.env.PREMIUM_NUMBER || '2348136118877'];
 
 global.typemenu = process.env.MENU_TYPE || 'v2';
 
